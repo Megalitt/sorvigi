ae0ae240981d2cd0f9998f51ad94727748f31fd4
Андрей Шастин <97834468+Megalitt@users.noreply.github.com>
2024-09-04 16:40:02 +0300

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	// basePath: "/sorvigi",
+	output: "export",
+	// reactStrictMode: true, 
+	// output: "standalone", 
+	// trailingSlash: true,
+};
+
+export default nextConfig;
